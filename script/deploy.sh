5c62e298c1c6165ad59cb6237cc701fb0438f30c
Frank Nocke <frank.nocke.extern@holidaycheck.com>
2017-11-24 16:56:14 +0100

    improve dploy script: safe use regardless of current folder

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
+# switch to script parent dir == project dir
+cd "$(dirname $(dirname $0))"
+
+if ! [[ "$PWD" =~ \/jekyll$ ]]; then
+	echo "you are not in the jekyll folder!"
+	exit 1
+fi
+
 read SITECREDS < ~/.ssh/sitecreds
 
 if [ -z "$SITECREDS" ]; then
-	echo "Could NOT read site credits. Exiting."
-	exit 666
+	echo "Could NOT read credits. Exiting."
+	exit 2
 fi
 
-echo "Starting deploy ------------------"
+echo "Starting JEKYLL deploy ------------------"
 
 lftp -c "set ftp:list-options -a; \
 	open $SITECREDS; \
